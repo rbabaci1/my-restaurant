c293093b10d3ff61451064e40792774730e88345
rbabaci1 <4rabah@gmail.com>
2022-11-30 17:18:04 -0800

    upload the featured images

--- components/Featured.js
+++ components/Featured.js
@@ -2,7 +2,27 @@ import Image from 'next/image';
 import styles from '../styles/Featured.module.css';
 
 function Featured() {
-  return <div className={styles.container}>Featured</div>;
+  const images = [
+    '/images/featured.png',
+    '/images/featured2.png',
+    '/images/featured3.png',
+  ];
+
+  return (
+    <div className={styles.container}>
+      <Image src='/images/arrowl.png' alt='' layout='fill' />
+
+      <div className={styles.wrapper}>
+        {images.map((img, i) => (
+          <div key={i} className={styles.imgContainer}>
+            <Image src={img} alt='' layout='fill' />
+          </div>
+        ))}
+      </div>
+
+      <Image src='/images/arrowr.png' alt='' layout='fill' />
+    </div>
+  );
 }
 
 export default Featured;
