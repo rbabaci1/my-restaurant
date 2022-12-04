fc0db4e52caae5647e0d542c817423cb88f720ba
rbabaci1 <4rabah@gmail.com>
2022-12-04 14:16:16 -0800

    add functionality to arrows to change images

--- components/Featured.js
+++ components/Featured.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import styles from '../styles/Featured.module.css';
 
@@ -8,22 +9,40 @@ function Featured() {
     '/images/featured3.png',
   ];
 
+  const [imgIndex, setImgIndex] = useState(0);
+
+  const handleClick = val => {
+    if ((imgIndex === 0 && val < 0) || (imgIndex === 2 && val > 0)) return;
+    setImgIndex(imgIndex + val);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.arrowContainer} style={{ left: 0 }}>
-        <Image src='/images/arrowl.png' alt='' layout='fill' />
+        <Image
+          src='/images/arrowl.png'
+          alt='back arrow'
+          layout='fill'
+          onClick={() => handleClick(-1)}
+        />
       </div>
 
       <div className={styles.wrapper}>
-        {images.map((img, i) => (
-          <div key={i} className={styles.imgContainer}>
-            <Image src={img} alt='' layout='fill' />
-          </div>
-        ))}
+        <Image
+          src={images[imgIndex]}
+          alt={`featured pizza ${imgIndex}`}
+          layout='fill'
+          objectFit='contain'
+        />
       </div>
 
       <div className={styles.arrowContainer} style={{ right: 0 }}>
-        <Image src='/images/arrowr.png' alt='' layout='fill' />
+        <Image
+          src='/images/arrowr.png'
+          alt='forward arrow'
+          layout='fill'
+          onClick={() => handleClick(+1)}
+        />
       </div>
     </div>
   );
