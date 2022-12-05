94b501d69494faa06c3cba1c6925710c5206b2d1
rbabaci1 <4rabah@gmail.com>
2022-12-05 12:27:28 -0800

    layout and style pizza card

--- components/PizzaCard.js
+++ components/PizzaCard.js
@@ -1,7 +1,23 @@
+import Image from 'next/image';
 import styles from '../styles/PizzaCard.module.css';
 
 function PizzaCard() {
-  return <div>PizzaCard</div>;
+  return (
+    <div className={styles.container}>
+      <Image
+        src='/images/pizza.png'
+        alt='pizza'
+        layout='responsive'
+        width={500}
+        height={500}
+      />
+      <h1 className={styles.title}>FIORI DI ZUCCA</h1>
+      <span className={styles.price}>$19.90</span>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+    </div>
+  );
 }
 
 export default PizzaCard;
